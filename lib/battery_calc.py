28ff34c38d68cf0a1e8ff83db819eb7a39f0e9c3
arctoz00 <131858739+arctoz00@users.noreply.github.com>
2023-11-05 13:47:38 +0100

    Add files via upload

--- lib/battery_calc.py
+++ lib/battery_calc.py
@@ -1,1 +1,29 @@
-l
+from machine import ADC, Pin
+
+analog_pin = ADC(Pin(34))
+analog_pin.atten(ADC.ATTN_11DB)
+
+
+def battery_percentage():
+    bat_measure = 3.3 / 4095
+    min_volt = 3.0
+    max_volt = 4.2
+    analog_val = analog_pin.read()
+    print ( "Analif målt værdi: ", analog_val)
+    #målt:
+    spænding = analog_val * bat_measure + 0.15 #Esp starter med at måle fra 0,15 volt af 
+    print ("spænding er: ", spænding)
+
+    Ubat = 3.95
+    Upin = 1.40
+
+    bo_er_go = Ubat / Upin
+
+    Batsp=bo_er_go * spænding
+
+    print("Batsp:",Batsp)
+
+    Batpct = 83.33 * Batsp -250
+    print("Batpct:",Batpct)
+    return Batpct
+      
---
+++ lib/credentials.py
@@ -0,0 +1,9 @@
+credentials = {
+    'ssid' : 'JESPERSPC',
+    'password' : '496xA501',
+    'ADAFRUIT_IO_URL' : b'io.adafruit.com',
+    'ADAFRUIT_USERNAME' : b'Jesperj',
+    'ADAFRUIT_IO_KEY' : b'aio_mWVj12JYCh9tjgtKkNUPGzdFZYVA',
+    'ADAFRUIT_IO_FEEDNAME' : b'mapfeed'
+    }  
+      
